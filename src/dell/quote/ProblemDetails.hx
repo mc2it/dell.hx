fb3ea26bec299bfe6dd711dd08b5e1ed5cc2d37f
Cédric Belin <cedric@belin.io>
2023-05-17 11:45:57 +0200

    Add the `ProblemDetails` model

---
+++ src/dell/quote/ProblemDetails.hx
@@ -0,0 +1,22 @@
+package dell.quote;
+
+import haxe.DynamicAccess;
+
+/** Provides the datails of a problem. **/
+@:jsonParse(json -> new dell.quote.ProblemDetails(json))
+@:jsonStringify(problem -> {
+	detail: problem.detail,
+	extensions: problem.extensions,
+	instance: problem.instance,
+	status: problem.status,
+	title: problem.title,
+	type: problem.type
+})
+class ProblemDetails implements Model {
+	@:constant var detail: String = @byDefault "";
+	@:constant var extensions: Null<DynamicAccess<Dynamic>> = null;
+	@:constant var instance: String = @byDefault "";
+	@:constant var status: Int = @byDefault 0;
+	@:constant var title: String = @byDefault "";
+	@:constant var type: String = @byDefault "";
+}
