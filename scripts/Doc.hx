aa67d4e5e49b50b132d151ee59af487a39a87223
Cédric Belin <cedric@belin.io>
2023-11-14 17:14:45 +0100

    Update the project URL

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -15,7 +15,7 @@ function main() {
 		"--define", "source-path", "https://github.com/mc2it/dell.hx/blob/main/src",
 		"--define", "themeColor", "0xea8220",
 		"--define", "version", Platform.packageVersion,
-		"--define", "website", "https://mc2it.github.io/dell.hx",
+		"--define", "website", "https://github.com/mc2it/dell.hx",
 		"--input-path", "var",
 		"--output-path", "docs/api",
 		"--title", "Dell API Client for Haxe",
